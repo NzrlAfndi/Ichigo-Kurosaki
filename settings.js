f1c272d4ffd5a76b84666505ad1d0683b5eb1b84
rolexgjhf <107097364+rolexgjhf@users.noreply.github.com>
2022-06-22 08:07:49 +0530

    settings.js

--- settings.js
+++ settings.js
@@ -9,8 +9,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Ubah Disini
-global.ownerNumber = ['6285609233482','6287877173955','0']
-global.ownerName = 'Fandyyy X All Creator'
+global.ownerNumber = ['8139011523','8139011523','0']
+global.ownerName = 'ARDIX RAVAN'
 global.packname = 'Ichigo Kurosaki'
 global.author = 'Whatsapp Bot 2022'
 global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
